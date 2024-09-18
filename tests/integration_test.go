b71dffeec59fc44b7da2eeb356ac01cd10736225
iabdukhoshimov <i.abdukhoshimov@gmail.com>
2024-09-18 12:54:48 +0500

    implemented test for pubsub and added github action ci

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -1,1 +1,51 @@
 package tests
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/iabdukhoshimov/pubsub-microservice-golang/pkg/handlers"
+	"github.com/iabdukhoshimov/pubsub-microservice-golang/pkg/pubsub"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPubSubIntegration(t *testing.T) {
+	ctx := context.Background()
+	client, err := pubsub.InitClient(ctx, "test-project")
+	assert.NoError(t, err)
+
+	// Create or get the topic
+	topic, err := pubsub.GetOrCreateTopic(ctx, client, "test-integration-topic")
+	assert.NoError(t, err)
+
+	// Publish a test message
+	messageData := []byte(`{
+		"id": "0001",
+		"type": "donut",
+		"name": "Cake",
+		"image": {"url": "images/0001.jpg", "width": 200, "height": 200},
+		"thumbnail": {"url": "images/thumbnails/0001.jpg", "width": 32, "height": 32}
+	}`)
+	err = pubsub.PublishMessage(ctx, topic, messageData)
+	assert.NoError(t, err)
+
+	// Create a subscription and start receiving messages
+	done := make(chan bool)
+
+	go func() {
+		err := pubsub.SubscribeToTopic(ctx, client, "test-integration-subscription", topic, func(data []byte) {
+			handlers.HandleMessage(data) // Handle message using the message handler
+			done <- true
+		})
+		assert.NoError(t, err)
+	}()
+
+	// Wait for the message to be processed
+	select {
+	case <-done:
+		// Test passes when the message is received and processed
+	case <-time.After(10 * time.Second):
+		t.Fatal("Test timed out, message not received")
+	}
+}
--- tests/pubsub_test.go
+++ tests/pubsub_test.go
@@ -1,1 +1,43 @@
 package tests
+
+import (
+	"context"
+	"testing"
+
+	pubsub "github.com/iabdukhoshimov/pubsub-microservice-golang/pkg/pubsub"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitClient(t *testing.T) {
+	ctx := context.Background()
+
+	// Test successful client creation
+	client, err := pubsub.InitClient(ctx, "test-project")
+	assert.NoError(t, err, "Error should be nil when initializing Pub/Sub client")
+	assert.NotNil(t, client, "Client should not be nil")
+}
+
+func TestGetOrCreateTopic(t *testing.T) {
+	ctx := context.Background()
+	client, err := pubsub.InitClient(ctx, "test-project")
+	assert.NoError(t, err)
+
+	// Test topic creation
+	topic, err := pubsub.GetOrCreateTopic(ctx, client, "test-topic")
+	assert.NoError(t, err, "Error should be nil when creating topic")
+	assert.NotNil(t, topic, "Topic should not be nil")
+}
+
+func TestPublishMessage(t *testing.T) {
+	ctx := context.Background()
+	client, err := pubsub.InitClient(ctx, "test-project")
+	assert.NoError(t, err)
+
+	topic, err := pubsub.GetOrCreateTopic(ctx, client, "test-topic")
+	assert.NoError(t, err)
+
+	// Test publishing a message
+	messageData := []byte(`{"id": "test-id", "type": "test-type"}`)
+	err = pubsub.PublishMessage(ctx, topic, messageData)
+	assert.NoError(t, err, "Error should be nil when publishing message")
+}
